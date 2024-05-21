18d8246fab2d9ab91195f87ba6b0b18b76b73e4d
tjfinite <tj.finite@gmail.com>
2024-05-21 11:02:55 +0200

    modified:   python3_application.py

--- python3_application.py
+++ python3_application.py
@@ -2,34 +2,49 @@ def vstupne_parametre():
     dochodkovy_vek = 86
     meno = input('Meno:')
     priezvisko = input("Priezvisko:")
-    while True: #Kontrola datoveho typu parametru vek
+    while True:  # Kontrola datoveho typu parametru vek
         vek = input("Vek:")
         try:
             vek = int(vek)
-            break
+            if vek >= 0:
+                break
+            else:
+                print("Vek musi byt kladne cislo!")
         except ValueError:
             print("Vek musi byt cele cislo!")
-    return meno, priezvisko, vek, dochodkovy_vek
+    while True:  # Kontrola datoveho typu parametru pocet_rokov
+        pocet_rokov = input('Pocet rokov:')
+        try:
+            pocet_rokov = int(pocet_rokov)
+            if pocet_rokov >= 0:
+                break
+            else:
+                print("Pocet rokov musi byt kladne cislo!")
+        except ValueError:
+            print("Pocet rokov musi byt cele cislo!")
+    return meno, priezvisko, vek, dochodkovy_vek, pocet_rokov
 
-def vystupne_parametre(meno, priezvisko, vek, dochodkovy_vek):
-    pocet_rokov_do_dochodku = dochodkovy_vek - vek
-    if vek < 18: #Kontrola veku do 18 rokov
-        print(f"{meno} este nie je v produktivnom veku.")
-    elif vek >= 86: #Kontrola dosiahnuteho dochodkoveho veku 
-        print(f"{meno} uz ma dochodkovy vek.")  
+def vystupne_parametre(meno, priezvisko, vek, dochodkovy_vek, pocet_rokov):
+    sucet_rokov = vek + pocet_rokov
+    if sucet_rokov > 85:  # Kontrola dochodkoveho veku
+        return "v dochodkovom veku."
+    elif sucet_rokov < 19:  # Kontrola veku do 18
+        return "mat menej ako 18 rokov."
     else:
-        stylizovane_slovo = stylizacia_slova_rok(pocet_rokov_do_dochodku)
-        print(f"{meno} {priezvisko} ma {vek} rokov a o {pocet_rokov_do_dochodku} {stylizovane_slovo} bude mat {dochodkovy_vek}, co je dochodkovy vek.")
-    return pocet_rokov_do_dochodku
+        return "v produktivnom veku."
 
-def stylizacia_slova_rok(pocet_rokov_do_dochodku):
-    if pocet_rokov_do_dochodku == 1:
-        stylizovane_slovo = "rok"
-    elif pocet_rokov_do_dochodku > 1 and pocet_rokov_do_dochodku < 5:
-        stylizovane_slovo = "roky"
+meno, priezvisko, vek, dochodkovy_vek, pocet_rokov = vstupne_parametre()
+vystup = vystupne_parametre(meno, priezvisko, vek, dochodkovy_vek, pocet_rokov)
+
+def stylistika(cislo):
+    if cislo == 1:
+        return "rok"
+    elif 1 < cislo < 5:
+        return "roky"
     else:
-        stylizovane_slovo = "rokov"
-    return stylizovane_slovo
+        return "rokov"
 
-meno, priezvisko, vek, dochodkovy_vek = vstupne_parametre()
-vystupne_parametre(meno, priezvisko, vek, dochodkovy_vek)
+slovo1 = stylistika(vek)
+slovo2 = stylistika(pocet_rokov)
+
+print(f"{meno} {priezvisko} ma {vek} {slovo1} a o {pocet_rokov} {slovo2} rokov bude {vystup}")
