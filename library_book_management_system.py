f5054b532b7f191a1018fd2af36ca063d449c32e
tjfinite <tj.finite@gmail.com>
2024-05-27 08:02:51 +0200

    for

--- library_book_management_system.py
+++ library_book_management_system.py
@@ -10,22 +10,29 @@ books = [
 library_info = ("LibraryName", "LibraryAddress")
 
 #Books availability
-book_availability = {
-    "Book1": True,
-    "Book2": False,
-    "Book3": False,
-    "Book4": True
-}
-
-#Print the books list
-print("Books:")
-for book in books:
-    print(book)
+book_availability = {book['title']: book['qty'] > 0 for book in books}
 
 #Print the library information
-print("\nLibrary information:")
-print (library_info)
+print ("Library name:",library_info[0])
+print ("Library address:",library_info[1])
+
+#Print the books names
+print("\nBooks:")
+for book in books:
+    print(book['title'])
+
+#Print the author names
+print("\nAuthors:")
+for book in books:
+    print(book['author'])
 
 #Print the books availability
 print("\nBooks availability:")
-print(book_availability)
+for title, available in book_availability.items():
+    status = "Yes" if available else "No"
+    print("{}: {}".format(title, status))
+
+#Print the books year
+print("\nBooks year:")
+for book in books:
+    print("{}: {}".format(book['title'], book['year']))
